1ffb7cb0fd322bf3450048942d346dda2f0e4705
Giorgio Zoppi <giorgio.zoppi@rstor.io>
2020-04-23 22:10:26 +0200

    Updates

---
+++ precache-manifest.48fdeab8b2fe9ca6cc2db1ec3e1e8031.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa7225b881c48d5382a31fa067d884e",
+    "url": "/noiapp/noiapp/index.html"
+  },
+  {
+    "revision": "dee63c9439abe6810e6d",
+    "url": "/noiapp/noiapp/static/css/main.95cbf666.chunk.css"
+  },
+  {
+    "revision": "92c0bc24c9f9fe507f31",
+    "url": "/noiapp/noiapp/static/js/2.c63a31a0.chunk.js"
+  },
+  {
+    "revision": "dee63c9439abe6810e6d",
+    "url": "/noiapp/noiapp/static/js/main.e3aeace8.chunk.js"
+  },
+  {
+    "revision": "38db14b4ab2be32fec71",
+    "url": "/noiapp/noiapp/static/js/runtime~main.869b3226.js"
+  },
+  {
+    "revision": "ada2c2f06b35745867b283d8237fb70e",
+    "url": "/noiapp/noiapp/static/media/italy.ada2c2f0.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/noiapp/noiapp/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/noiapp/noiapp/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/noiapp/noiapp/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/noiapp/noiapp/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
